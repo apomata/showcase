1ec30f1ab8554980e575c3b9eb32ac599d3418cd
alex pomata <apomata@brandeis.edu>
2014-09-18 01:27:45 -0400

    fixed projects model admin working

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,7 +1,25 @@
 RailsAdmin.config do |config|
   config.main_app_name = ["showcase",]
 
-  config.attr_accessible_role { _current_user.role.to_sym }
+  #config.attr_accessible_role { :default }
 
   config.label_methods << :description # Default is [:name, :title]
+
+  config.actions do
+    # root actions
+    dashboard                     # mandatory
+    # collection actions 
+    index                         # mandatory
+    new
+    export
+    history_index
+    bulk_delete
+    # member actions
+    show
+    edit
+    delete
+    history_show
+    show_in_app
+  end
+
 end
